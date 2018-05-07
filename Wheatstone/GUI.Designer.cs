40029289b93311ad8e6e1072847fd0a2c8c93e80
Christian Wichmann <wichmann1988@gmx.de>
2018-05-08 01:58:55 +0200

    Calculated points are now properly mapped to the output box.

--- Wheatstone/GUI.Designer.cs
+++ Wheatstone/GUI.Designer.cs
@@ -32,75 +32,122 @@ namespace Wheatstone
         private void InitializeComponent()
         {
             this.diagramBox = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.buttonToDrawGraph = new System.Windows.Forms.Button();
+            this.buttonToClearImage = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.diagramBox)).BeginInit();
             this.SuspendLayout();
             // 
             // diagramBox
             // 
-            this.diagramBox.Location = new System.Drawing.Point(12, 12);
+            this.diagramBox.BackColor = System.Drawing.Color.White;
+            this.diagramBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.diagramBox.Location = new System.Drawing.Point(80, 80);
             this.diagramBox.Name = "diagramBox";
             this.diagramBox.Size = new System.Drawing.Size(500, 500);
             this.diagramBox.TabIndex = 0;
             this.diagramBox.TabStop = false;
             this.diagramBox.Paint += new System.Windows.Forms.PaintEventHandler(this.diagramBox_Paint);
-            this.diagramBox.BackColor = Color.White;
-            this.diagramBox.BorderStyle = BorderStyle.FixedSingle;
-
             // 
-            // button1
+            // buttonToDrawGraph
             // 
-            this.button1.Location = new System.Drawing.Point(658, 111);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.buttonToDrawGraph.Location = new System.Drawing.Point(586, 80);
+            this.buttonToDrawGraph.Name = "buttonToDrawGraph";
+            this.buttonToDrawGraph.Size = new System.Drawing.Size(108, 23);
+            this.buttonToDrawGraph.TabIndex = 2;
+            this.buttonToDrawGraph.Text = "Draw graph";
+            this.buttonToDrawGraph.UseVisualStyleBackColor = true;
+            this.buttonToDrawGraph.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button2
+            // buttonToClearImage
             // 
-            this.button2.Location = new System.Drawing.Point(658, 169);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.buttonToClearImage.Location = new System.Drawing.Point(586, 166);
+            this.buttonToClearImage.Name = "buttonToClearImage";
+            this.buttonToClearImage.Size = new System.Drawing.Size(108, 23);
+            this.buttonToClearImage.TabIndex = 3;
+            this.buttonToClearImage.Text = "Clear diagram";
+            this.buttonToClearImage.UseVisualStyleBackColor = true;
+            this.buttonToClearImage.Click += new System.EventHandler(this.button3_Click);
             // 
-            // button3
+            // textBox1
             // 
-            this.button3.Location = new System.Drawing.Point(658, 223);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.textBox1.Location = new System.Drawing.Point(561, 586);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(43, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "10000";
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyUp);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(586, 271);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(108, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "4000";
+            this.textBox2.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(586, 255);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "unknown resisitor in Ohm";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "5V";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 567);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "-5V";
             // 
             // GUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(877, 646);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonToClearImage);
+            this.Controls.Add(this.buttonToDrawGraph);
             this.Controls.Add(this.diagramBox);
             this.Name = "GUI";
             this.Text = "GUI";
             ((System.ComponentModel.ISupportInitialize)(this.diagramBox)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox diagramBox;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button buttonToDrawGraph;
+        private System.Windows.Forms.Button buttonToClearImage;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
     }
 }
--- Wheatstone/GUI.cs
+++ Wheatstone/GUI.cs
@@ -1,35 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace Wheatstone
 {
     public partial class GUI : Form
     {
+        private float valueRange;
+        private float unknownResistor;
+
         public GUI()
         {
+            this.valueRange = 10000;
+            this.unknownResistor = 6000;
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            DrawLine(0);
         }
 
         private void diagramBox_Paint(object sender, PaintEventArgs e)
         {
             for (int i = 1; i <= 5; i++)
             {
-                e.Graphics.DrawLine(new Pen(Color.Black, 1), i * 100, 0, i * 100, 500);
-                e.Graphics.DrawLine(new Pen(Color.Black, 1), 0, i * 100, 500, i * 100);
+                e.Graphics.DrawLine(new Pen(Color.Beige, 1), i * 100, 0, i * 100, 500);
+                e.Graphics.DrawLine(new Pen(Color.Beige, 1), 0, i * 100, 500, i * 100);
             }
-        }
-
-        public void DrawLine(int bar)
-        {
-            var foo = this.diagramBox.CreateGraphics();
-            foo.DrawLine(new Pen(Color.Black), bar, 0,500,500);
+            e.Graphics.DrawLine(new Pen(Color.Black, 1), 0, 250, 500, 250);
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -40,41 +35,80 @@ namespace Wheatstone
         private void DrawCurve()
         {
             var points = CalculatePoints();
-            var diffY = points[0].Y - points[499].Y;
-            var lowestY = points[499].Y;
-
-            for (int i = 0; i < 500; i++)
-            {
-                points[i].Y += (lowestY*-1);
-                points[i].Y *= 500/diffY;
-            }
+            var zeroPoint = points.FirstOrDefault(p => Math.Abs(p.Y - 250) < 0.1);
 
             var foo = this.diagramBox.CreateGraphics();
-            foo.DrawCurve(new Pen(Color.Aqua),points );
+            foo.DrawCurve(new Pen(Color.Crimson), points);
+            if (Math.Abs(zeroPoint.Y) > 0.01)
+            {
+                foo.DrawLine(new Pen(Color.Black), 0, zeroPoint.Y, 500, zeroPoint.Y);
+            }
         }
 
         private PointF[] CalculatePoints()
         {
             var result = new PointF[500];
+            var builder = new PointBuilder(this.valueRange, this.unknownResistor);
             for (int i = 0; i < 500; i++)
             {
-                result[i] = new PointF(i,CalculateCurrent(i));
+                result[i] = builder.Build(i);
             }
             return result;
         }
 
-        private float CalculateCurrent(int i)
-        {
-            float baseCurrent = 10;
-            float staticResistor = 10000;
-            float variableResistor = 6000;
-            return baseCurrent * ((variableResistor * staticResistor - staticResistor * i*20) /
-                                          ((variableResistor + staticResistor) * (staticResistor + i*20)));
-        }
+
 
         private void button3_Click(object sender, EventArgs e)
         {
             this.diagramBox.Invalidate();
+        }
+
+        private void textBox2_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode.Equals(Keys.Enter))
+            {
+                this.unknownResistor = Convert.ToSingle(textBox2.Text);
+                DrawCurve();
+            }
+        }
+
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode.Equals(Keys.Enter))
+            {
+                this.valueRange = Convert.ToSingle(textBox1.Text);
+                DrawCurve();
+            }
+        }
+    }
+
+    public class PointBuilder
+    {
+        private readonly float valueRange;
+        private readonly float unknownResistor;
+
+        public PointBuilder(float valueRange, float unknownResistor)
+        {
+            this.valueRange = valueRange;
+            this.unknownResistor = unknownResistor;
+        }
+
+        public PointF Build(int iteration)
+        {
+            return new PointF
+            {
+                X = iteration,
+                Y = 500 - (50 * CalculateCurrent(iteration) + 250)
+            };
+        }
+
+        private float CalculateCurrent(int iteration)
+        {
+            var variableResistor = iteration * this.valueRange / 500;
+            const float baseCurrent = 10;
+            const float staticResistor = 10000;
+            return baseCurrent * ((staticResistor * this.unknownResistor - staticResistor * variableResistor) /
+                                  ((staticResistor + staticResistor) * (this.unknownResistor + variableResistor)));
         }
     }
 }
